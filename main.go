f1381b9ad88a095136d0acf26521ca7e2815cbe7
Joost <github@joostnieuweboer.nl>
2024-12-19 11:47:44 +0100

    First console command version stub

--- main.go
+++ main.go
@@ -12,7 +12,17 @@ func main() {
 	//get console arguments
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text()) // Println will add back the final '\n'
+		//fmt.Println(scanner.Text()) // Println will add back the final '\n'
+		input := clean_input(scanner.Text())
+		//get the command
+		command := input[0]
+		//get the arguments
+		arguments := input[1:]
+		
+		//print the split arguments
+		fmt.Println("Command: ", command)
+		fmt.Println("Argument(s): ", arguments)	
+
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
@@ -27,20 +37,12 @@ func main() {
 function that sanitized the console command to seperate topics
 https://pkg.go.dev/strings#Fields
 */
-func clean_input(text string) string, []string  {
+func clean_input(text string) []string  {
 	//convert to lower case
-	input_lower_case := strings.toLower(text)
+	input_lower_case := strings.ToLower(text)
 	//arguments
-	split_text := strings.fields(input_lower_case)
-	//get the command
-	command := split_text[0]
-	//get the arguments
-	arguments := split_text[1:]
-	
-	//print the split arguments
-	fmt.Println("Command: ", command)
-	fmt.Println("Argument(s): ", arguments)	
-	
+	split_text := strings.Fields(input_lower_case)
 	//split up the string into seperate strings using whitespaces
-	return command, arguments
+	return split_text
 }
+
